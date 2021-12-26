400fe27849b48952a5442a083cf33f1061ef0932
anycad <anycad@qq.com>
2021-12-26 19:28:17 +0800

    add VB.NET

---
+++ VB.WinForms.NET6/VB.WinFormsStarter/Form1.vb
@@ -0,0 +1,15 @@
+﻿Imports AnyCAD.Foundation
+
+Public Class Form1
+
+    Private mRenderView As AnyCAD.Forms.RenderControl
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GlobalInstance.Initialize()
+        mRenderView = New AnyCAD.Forms.RenderControl(SplitContainer1.Panel2)
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        GlobalInstance.Destroy()
+    End Sub
+End Class
