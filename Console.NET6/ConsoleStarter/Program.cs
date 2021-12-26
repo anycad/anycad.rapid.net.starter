f802cc0aaead6963c7c410ffd63b8cae9ed15614
anycad <anycad@qq.com>
2021-12-26 19:46:38 +0800

    add Console sample

---
+++ Console.NET6/ConsoleStarter/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AnyCAD.Foundation;
+
+GlobalInstance.Initialize();
+
+WindowCanvas canvas = new WindowCanvas("AnyCAD", true);
+canvas.Initialize(0, 600, 400);
+
+var box = ShapeBuilder.MakeBox(GP.XOY(), 10, 20, 30);
+var node = BrepSceneNode.Create(box, null, null, 0.01);
+var scene = canvas.GetContext().GetScene();
+scene.AddNode(node);
+canvas.Run();
+
+canvas.Destroy();
+
+GlobalInstance.Destroy();
