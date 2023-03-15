d81102b8aa750d616a782cd8fd3feaf5e46d1606
cloud <cloud@qq.com>
2023-03-15 17:51:09 +0800

    .NET 4.8

---
+++ WinForms.NET4/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AnyCAD.Forms;
+namespace WinForms.NET4
+{
+    public partial class Form1 : Form
+    {
+        RenderControl mRenderView;
+        public Form1()
+        {
+            InitializeComponent();
+            mRenderView = new RenderControl(this.panel1);
+        }
+    }
+}
