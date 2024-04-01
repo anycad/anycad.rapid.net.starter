f5c8b28a8834d6a256ff184a4296332f512e50c3
malw <anycad@qq.com>
2024-04-01 12:07:03 +0800

    .NET 4.7.2

---
+++ WinForms.NET472/Form1.cs
@@ -0,0 +1,25 @@
+﻿using AnyCAD.Forms;
+using AnyCAD.Foundation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms.NET472
+{
+    public partial class Form1 : Form
+    {
+        IRenderView _Render;
+        public Form1()
+        {
+            InitializeComponent();
+
+            _Render = new RenderControl(this.splitContainer1.Panel2);
+        }
+    }
+}
